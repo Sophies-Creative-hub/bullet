3590c8bf213260a89219d746dab2e8bdec97ccdf
Sophie Ischenko <82713289+Sophies-Creative-hub@users.noreply.github.com>
2023-12-01 17:08:14 +0100

    Update test.js

--- src/test.js
+++ src/test.js
@@ -1,30 +1,30 @@
-require("chromedriver");
-
-const { Builder, By, Key } = require("selenium-webdriver");
-const assert = require("assert");
+const { Builder, By, Key, Capabilities } = require("selenium-webdriver");
+require("chromedriver"); // Make sure chromedriver is installed and available in your project
 
 async function runTest() {
   // Set up Selenium WebDriver with Chrome in headless mode
   const chromeCapabilities = Capabilities.chrome();
   chromeCapabilities.set('goog:chromeOptions', { args: ['--headless'] });
-  const driver = new webdriver.Builder().withCapabilities(chromeCapabilities).build();
+  const driver = new Builder().withCapabilities(chromeCapabilities).build();
 
   try {
     // Open the local website
     await driver.get("https://victoria-lo.github.io/bulletin-board/"); // Update the URL to your local server
 
-    //find the search box and enter a note
+    // Find the search box and enter a note
     await driver.findElement(By.xpath('//*[@id="new-item"]/input')).sendKeys("Hello Selenium", Key.RETURN);
 
-    //get the note's text
+    // Get the note's text
     let note = await driver.findElement(By.xpath('//*[@id="items"]/div/p')).getText();
 
-    //assert that the note's text is the same as the input text "Hello Selenium"
+    // Assert that the note's text is the same as the input text "Hello Selenium"
     assert.strictEqual(note, "Hello Selenium");
     console.log("TEST PASSED");
 
   } finally {
-    //close the browser
+    // Close the browser
     await driver.quit();
   }
-})();
+}
+
+runTest();
