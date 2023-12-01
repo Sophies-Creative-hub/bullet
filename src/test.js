4909612ed8486266244425079a0934fa240885a5
sophie <s.ischenko1990@gmail.com>
2023-12-01 13:46:55 +0100

    initial

---
+++ src/test.js
@@ -0,0 +1,28 @@
+require("chromedriver");
+
+const { Builder, By, Key } = require("selenium-webdriver");
+const assert = require("assert");
+
+(async function example() {
+  // Open Chrome browser
+  let driver = await new Builder().forBrowser("chrome").build();
+
+  try {
+    // Open the local website
+    await driver.get("http://localhost:3000"); // Update the URL to your local server
+
+    //find the search box and enter a note
+    await driver.findElement(By.xpath('//*[@id="new-item"]/input')).sendKeys("Hello Selenium", Key.RETURN);
+
+    //get the note's text
+    let note = await driver.findElement(By.xpath('//*[@id="items"]/div/p')).getText();
+
+    //assert that the note's text is the same as the input text "Hello Selenium"
+    assert.strictEqual(note, "Hello Selenium");
+    console.log("TEST PASSED");
+
+  } finally {
+    //close the browser
+    await driver.quit();
+  }
+})();
