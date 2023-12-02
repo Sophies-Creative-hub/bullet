390a8e1bc4dce759890de8de0bca60a9a266154b
Sophie Ischenko <82713289+Sophies-Creative-hub@users.noreply.github.com>
2023-12-02 20:35:37 +0100

    Update test.js

--- src/test.js
+++ src/test.js
@@ -1,31 +1,31 @@
 const { Builder, By, Key, Capabilities } = require("selenium-webdriver");
 require("chromedriver");
-const assert = require('chai').assert; // Fügen Sie diese Zeile hinzu
+const assert = require('chai').assert;
 
-async function runTest() {
-  // Set up Selenium WebDriver with Chrome in headless mode
-  const chromeCapabilities = Capabilities.chrome();
-  chromeCapabilities.set('goog:chromeOptions', { args: ['--headless'] });
-  const driver = new Builder().withCapabilities(chromeCapabilities).build();
+describe("Selenium Tests", function () {
+  it("should add a note and display on the page", async function () {
+    // Set up Selenium WebDriver with Chrome in headless mode
+    const chromeCapabilities = Capabilities.chrome();
+    chromeCapabilities.set('goog:chromeOptions', { args: ['--headless'] });
+    const driver = new Builder().withCapabilities(chromeCapabilities).build();
 
-  try {
-    // Open the local website
-    await driver.get("https://victoria-lo.github.io/bulletin-board/");
+    try {
+      // Open the local website
+      await driver.get("https://victoria-lo.github.io/bulletin-board/");
 
-    // Find the search box and enter a note
-    await driver.findElement(By.xpath('//*[@id="new-item"]/input')).sendKeys("Hello Selenium", Key.RETURN);
+      // Find the search box and enter a note
+      await driver.findElement(By.xpath('//*[@id="new-item"]/input')).sendKeys("Hello Selenium", Key.RETURN);
 
-    // Get the note's text
-    let note = await driver.findElement(By.xpath('//*[@id="items"]/div/p')).getText();
+      // Get the note's text
+      let note = await driver.findElement(By.xpath('//*[@id="items"]/div/p')).getText();
 
-    // Assert that the note's text is the same as the input text "Hello Selenium"
-    assert.strictEqual(note, "Hello Selenium");
-    console.log("TEST PASSED");
+      // Assert that the note's text is the same as the input text "Hello Selenium"
+      assert.strictEqual(note, "Hello Selenium");
+      console.log("TEST PASSED");
 
-  } finally {
-    // Close the browser
-    await driver.quit();
-  }
-}
-
-runTest();
+    } finally {
+      // Close the browser
+      await driver.quit();
+    }
+  });
+});
