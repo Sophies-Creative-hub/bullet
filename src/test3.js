df159b09e75c82a71ca9c0c3382a6e08caea6afc
Sophie Ischenko <82713289+Sophies-Creative-hub@users.noreply.github.com>
2024-03-20 11:55:53 +0100

    Update test3.js

--- src/test3.js
+++ src/test3.js
@@ -1,4 +1,4 @@
-require("geckodriver"); // Ändern Sie hier "chromedriver" zu "geckodriver"
+require("geckodriver"); 
 
 const { Builder, By, Key } = require("selenium-webdriver");
 var assert = require("chai").assert;
@@ -8,15 +8,15 @@ describe("add note", function () {
     //it - describes expected behaviour
     it("should add a note and display on the page", async function () {
         /*Selenium automates:
-       1. Open Firefox // Hier wurde der Kommentar für Klarheit angepasst
+       1. Open Firefox 
        2. Navigate to app
        3. Type "Hello Selenium" in input box
        4. Clicks the Enter key
       */
         //Chai asserts if new note's text matches the input
 
-        //open Firefox browser // Hier wurde der Kommentar für Klarheit angepasst
-        let driver = await new Builder().forBrowser("firefox").build(); // Hier wurde "chrome" durch "firefox" ersetzt
+        //open Firefox browser 
+        let driver = await new Builder().forBrowser("firefox").build(); 
 
         try {
             //open the website
