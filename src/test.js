d96fe166db65eabadd1b805869b6c39c51b47a2f
Sophie Ischenko <82713289+Sophies-Creative-hub@users.noreply.github.com>
2024-03-20 11:44:29 +0100

    Update test.js

--- src/test.js
+++ src/test.js
@@ -1,23 +1,25 @@
 const { Builder, By, Key, Capabilities } = require("selenium-webdriver");
-require("chromedriver");
+require("geckodriver");
 const assert = require('chai').assert;
 
 describe("Selenium Tests", function () {
   it("should add a note and display on the page", async function () {
-    // Set up Selenium WebDriver with Chrome in headless mode
-    const chromeCapabilities = Capabilities.chrome();
-    chromeCapabilities.set('goog:chromeOptions', { args: ['--headless'] });
-    const driver = new Builder().withCapabilities(chromeCapabilities).build();
+    // Set up Selenium WebDriver with Firefox in headless mode
+    const firefoxCapabilities = Capabilities.firefox();
+    firefoxCapabilities.set('moz:firefoxOptions', { args: ['-headless'] });
+    const driver = new Builder().withCapabilities(firefoxCapabilities).build();
 
     try {
       // Open the local website
       await driver.get("http://localhost:3000");
 
       // Find the search box and enter a note
-      await driver.findElement(By.xpath('//*[@id="new-item"]/input')).sendKeys("Hello Selenium", Key.RETURN);
+      await driver.findElement(By.xpath('//*[@id="new-item"]/input'))
+                  .sendKeys("Hello Selenium", Key.RETURN);
 
       // Get the note's text
-      let note = await driver.findElement(By.xpath('//*[@id="items"]/div/p')).getText();
+      let note = await driver.findElement(By.xpath('//*[@id="items"]/div/p'))
+                            .getText();
 
       // Assert that the note's text is the same as the input text "Hello Selenium"
       assert.strictEqual(note, "Hello Selenium");
